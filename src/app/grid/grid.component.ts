762f34cf84ef1c316386bacce6460d25e5e0a89a
Lavate <pooja.lavate@worldline.com>
2023-11-29 15:46:49 +0530

    added delete button

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocationDataService } from '../services/location-data.service';
 import { Observable } from 'rxjs/internal/Observable';
 import { HttpClient } from '@angular/common/http';
@@ -7,21 +7,77 @@ import { Subscribable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css'],
   providers: [LocationDataService]
 })
-export class GridComponent{
-  // locations: Location[] = []; // Initialize locations array
-  // LocationModel: any;
-  Location: any;
+export class GridComponent implements OnInit{
+  locations: LocationModel[] = []; // Initialize locations array
+  isAddButtonEnabled: boolean = true;
+  isDeleteButtonEnabled = false;
+  isCancelButtonEnabled: boolean = false;
+  // enableDeleteButton: boolean = false;
+  isChecked: boolean = false;
+ 
+
+  enableButton(checked: boolean) {
+    this.isChecked = checked;
+  }
+  
+  deleteCheckedRows() {
+    this.locations = this.locations.filter(location => !location.selected);
+    // Reset the isChecked flag after deletion
+    this.isChecked = false;
+  }
+  
+  // onDeleteClick() {
+   
+  //   alert('Delete button clicked!')
+  //   // this.locations = this.locations.filter(location => !location.selected);
+  //   // this.isDeleteButtonEnabled = false; // Disable delete button after deletion
+  // }
+
+onAddClick() {
+  alert('Add button clicked!');
+  this.isAddButtonEnabled = false;
+  this.isDeleteButtonEnabled=false;
+  this.isCancelButtonEnabled = true;
+   // Create a new row and add it at the top of the grid
+   const newLocation: LocationModel = {
+    nlcCode: 'New NLC Code',
+    crsCode: 'New CRS Code',
+    description: 'New Description',
+    selected: false,
+  };
+  this.locations$ = this.locations$.pipe(
+    map((locations: LocationModel[]) =>[newLocation, ...locations])
+    );
+    // Remove the first element
+  
+}
+
+// onDeleteClick(){
+//   alert('Delete button clicked!');
+//   this.isDeleteButtonEnabled=false;
+// }
+onCancelClick(){
+  alert('Cancel button clicked!');
+  this.isAddButtonEnabled = true;
+  this.isCancelButtonEnabled = false;
+  this.isDeleteButtonEnabled=true;
+  this.locations$ = this.locations$.pipe(
+    map((locations: LocationModel[]) => locations.slice(1)) // Remove the first element
+  );
+}
+
+
+
+
+Location: any;
 locations$!: Observable<LocationModel[]>;
-
-//   locations!: any; 
-// locations$: Observable<undefined>|Subscribable<undefined>|Promise<undefined>;
-
 
   constructor(private ldservice:LocationDataService){
 
@@ -31,6 +87,7 @@ locations$!: Observable<LocationModel[]>;
    
  ngOnInit():void{
   this.locations$ =  this.ldservice.getLocations()
+  // this.enableDeleteButton = false;
  }
   goBack(): void {
     window.history.back(); // Implement your logic for going back
